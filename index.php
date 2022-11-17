62b4ea41ed5b02ebb990e5fd06d3c87c2452eec5
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-11-17 15:15:56 +0700

    add css, list siswa , index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 <head>
     <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
--- list-siswa.php
+++ list-siswa.php
@@ -1,1 +1,66 @@
 <!-- halaman untuk menampilkan data siswa -->
+<?php include("config.php"); ?> <!-- Untuk koneksi ke dp -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <header>
+        <h3>Siswa yang sudah mendaftar</h3>
+    </header>
+
+    <nav>
+        <a href="form-daftar.php">[+] Tambah Baru</a>
+    </nav>
+
+    <br>
+
+    <table border="1">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Jenis Kelamin</th>
+            <th>Agama</th>
+            <th>Sekolah Asal</th>
+            <th>Tindakan</th>
+        </tr>
+    </thead>
+    <tbody>
+
+        <?php
+        $sql = "SELECT * FROM calon_siswa";
+        $query = mysqli_query($db, $sql);
+        $total_siswa = 0;
+        while($siswa = mysqli_fetch_array($query)){
+            echo "<tr>";
+
+            echo "<td>".$siswa['id']."</td>";
+            echo "<td>".$siswa['nama']."</td>";
+            echo "<td>".$siswa['alamat']."</td>";
+            echo "<td>".$siswa['jenis_kelamin']."</td>";
+            echo "<td>".$siswa['agama']."</td>";
+            echo "<td>".$siswa['sekolah_asal']."</td>";
+
+            echo "<td>";
+            echo "<a href='form-edit.php?id=".$siswa['id']."'>Edit</a> | ";
+            echo "<a href='hapus.php?id=".$siswa['id']."'>Hapus</a>";
+            echo "</td>";
+
+            echo "</tr>";
+            $total_siswa+=1;
+        }
+        ?>
+
+    </tbody>
+    </table>
+
+    <p>Total: <?php echo $total_siswa ?></p>
+    <a href="index.php">Kembali</a>
+    </body>
+</html>
