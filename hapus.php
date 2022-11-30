65df7f8a05a2266da3e1a4ec9d79420952796e4d
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-11-30 08:54:27 +0700

    ad delete upload

--- hapus.php
+++ hapus.php
@@ -8,6 +8,21 @@ if( isset($_GET['id']) ){
     // ambil id dari query string
     $id = $_GET['id'];
 
+    $sql = "SELECT * FROM calon_siswa WHERE id=$id";
+    $query = mysqli_query($db, $sql);
+    if($query){
+        $siswa = mysqli_fetch_array($query);
+        $infodebug='aa';
+        // Cek apakah file foto sebelumnya ada di folder images
+        if(is_file($siswa['path_foto'])){
+            // Jika foto ada
+            unlink($siswa['foto']); // Hapus file foto sebelumnya yang ada di folder images
+            $infodebug="berhasilunlink";
+        }else{
+            $infodebug="bukanfile".$siswa['path_foto'];
+        }
+    }
+
     // buat query hapus
     $sql = "DELETE FROM calon_siswa WHERE id=$id";
     $query = mysqli_query($db, $sql);
