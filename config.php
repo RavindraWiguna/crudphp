7e0198f5a6f8a13f97ee18bb1e6bee2edbca793e
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-12-07 14:59:14 +0700

    fix config error with fpdf

--- config.php
+++ config.php
@@ -1,17 +1,13 @@
-<!-- Menyimpan configurasi database -->
 <?php
 
 $server = "localhost";
 $user = "myroot";
-$password = "charmander1";
+$password = "1234567890";
 $nama_database = "pendaftaran_siswa";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
 if(!$db){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
-}
-else{
-    // echo "Berhasil terhusbung ke database";
 }
 ?>
